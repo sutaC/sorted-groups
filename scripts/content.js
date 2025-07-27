6024dc8f7b3ff4ac93cf81cb1e0ebb53eaa6b168
sutaC <catus.pl@gmail.com>
2025-07-27 15:08:25 +0200

    Handles SPA redirects

--- scripts/content.js
+++ scripts/content.js
@@ -3,19 +3,33 @@ const valid_settings = /** @type {const} */ ([
     "RECENT_ACTIVITY",
     "CHRONOLOGICAL",
 ]);
-(async () => {
+const setDefaultSorting = async () => {
+    if (!location.pathname.startsWith("/groups/")) return;
     const params = new URLSearchParams(location.search);
-    if (!params.has("sorting_setting")) {
-        let { default_sorting_setting } = await chrome.storage.sync.get(
-            "default_sorting_setting"
-        );
-        if (!valid_settings.includes(default_sorting_setting)) {
-            default_sorting_setting = valid_settings[0];
-            await chrome.storage.sync.set({
-                default_sorting_setting: default_sorting_setting,
-            });
-        }
-        params.append("sorting_setting", default_sorting_setting);
-        location.search = params.toString();
+    if (params.has("sorting_setting")) return;
+    let { default_sorting_setting } = await chrome.storage.sync.get(
+        "default_sorting_setting"
+    );
+    if (!valid_settings.includes(default_sorting_setting)) {
+        default_sorting_setting = valid_settings[0];
+        await chrome.storage.sync.set({
+            default_sorting_setting: default_sorting_setting,
+        });
     }
-})();
+    params.append("sorting_setting", default_sorting_setting);
+    location.search = params.toString();
+};
+setDefaultSorting();
+// --- Handles SPA redirects
+let lastUrl = location.href;
+const checkUrlChange = () => {
+    const currentUrl = location.href;
+    if (currentUrl === lastUrl) return;
+    lastUrl = currentUrl;
+    setDefaultSorting();
+};
+const observer = new MutationObserver(checkUrlChange.bind(this));
+observer.observe(document, {
+    subtree: true,
+    childList: true,
+});
